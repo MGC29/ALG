6b817066844182e36d693f5c3d6709bb0d3efc62
Montse <mgcerqueda@gmail.com>
2024-10-29 22:52:39 +0100

    Update peek2.sh

--- Peek/peek2.sh
+++ Peek/peek2.sh
@@ -1,5 +1,6 @@
-# This file outputs the first and last number of required lines of a file with three dots in between. 
-# In case the file is empty or there are less than 3 lines, it just outputs blank spaces or the total number of lines.
+# This file outputs the first and last number of required lines of a file with three dots in between.
+# If the number of lines is not stated, it prints 3 lines by default 
+# In case the file is empty or there are less than stated number of lines, it just outputs blank spaces or the total number of lines.
 
 #This function checks that the argument exists and is a readable file. 
 check_if_file(){
@@ -23,31 +24,37 @@ if [[ ! "$1" =~ ^[0-9]+$ ]]; then
 fi
 }
 
-#This function checks that both arguments (fle and number) are present and correct. 
+#This function checks that both arguments (file and number) are correct. 
 check_input(){
 if [ -z "$1" ]; then
     echo "Error: you are missing the first argument." >&2
     echo "Please provide a file to check." >&2
     exit 1
 elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    echo "Usage: peek [FILE]"
     echo "Usage: peek [FILE] [NUMBER]"
     echo "Prints the first and last NUMBER of lines of the FILE with three dots in between."
+    echo "If the NUMBER is not stated, it prints 3 lines by default."
     echo "If NUMBER is 0, it just prints the three dots. If NUMBER is higher than the number of lines in FILE, it prints the maximum number of lines."
     exit 1
 else
     check_if_file $1
 fi
-
-if [ -z "$2" ]; then
-    echo "Error: you are missing the second argument." >&2
-    echo "Please provide a number of lines to print." >&2
-    exit 1
-else 
+if [ ! -z "$2" ]; then
     check_if_number $2
 fi 
 }
 
 check_input $1 $2
-head -n $2 $1 
-echo "..."
-tail -n $2 $1
+
+if [[ -z "$2" ]]; then
+    head -n 3 $1 
+    echo "..."
+    tail -n 3 $1
+else
+    head -n $2 $1 
+    echo "..."
+    tail -n $2 $1    
+fi 
+
+

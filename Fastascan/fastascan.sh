e7609fc4b448a4298056e8416e2c19c6ae0479fd
Montse <mgcerqueda@gmail.com>
2024-11-26 23:05:02 +0100

    Update file

--- Fastascan/fastascan.sh
+++ Fastascan/fastascan.sh
@@ -3,7 +3,7 @@
     # 2. a number of lines, here called N (default: 0)
 # The report should include this information:
     # how many such files there are (DONE)
-    # how many unique fasta IDs (i.e. the first words of fasta headers) they contain in total 
+    # how many unique fasta IDs (i.e. the first words of fasta headers) they contain in total (DONE? ASK)
     # for each file:
     # print a nice header including filename; and:
         # whether the file is a symlink or not
@@ -13,45 +13,67 @@
         # next, if the file has 2N lines or fewer, then display its full content; if not, show the first N lines, then "...", then the last N lines. If N is 0, skip this step.
 
 check_args(){
-if [[ -d $1 ]]; then 
-    if [[ $DIR == '.' ]]; then 
-        if [[ -r $1 ]]; then
-            DIR=$1
-        else
-            echo "I don't have permission to check this directory." >&2
-            exit 4
+    if [[ -d $1 ]]; then 
+        if [[ $DIR == '.' ]]; then 
+            if [[ -r $1 ]]; then
+                DIR=$1
+            else
+                echo "I don't have permission to check this directory." >&2
+                exit 1
+            fi
+        else echo "You have provided two directories to check. By default the first one will be used." >&2
         fi
-    else echo "You have provided two directories to check. By default the first one will be used." >&2
-    fi
-elif [[ "$1" == "-h" || "$1" == "--help" ]]; then 
-    echo "Usage: fastascan [DIRECTORY] [LINES]"
-    exit 0    
-elif [[ $1 =~ ^[0-9]+$ ]]; then 
-    if [[ $N_LINES -eq 0 ]]; then 
-        N_LINES=$1 
-    else echo "You have provided two number of lines to check. By default the first one will be used." >&2
-    fi
-else 
-    echo "The program only takes two arguments: the directory to check and the number of lines to print. "
-    echo "The argument" $1 "is incorrect."
-    exit 3
-fi 
+    elif [[ "$1" == "-h" || "$1" == "--help" ]]; then 
+        echo "Usage: fastascan [DIRECTORY] [LINES]"
+        exit 0    
+    elif [[ $1 =~ ^[0-9]+$ ]]; then 
+        if [[ $N_LINES -eq 0 ]]; then 
+            N_LINES=$1 
+        else echo "You have provided two number of lines to check. By default the first one will be used." >&2
+        fi
+    else 
+        echo "The program only takes two arguments: the directory to check and the number of lines to print. "
+        echo "The argument" $1 "is incorrect."
+        exit 1
+    fi 
 }
-
-if [[ $# -gt 2 ]]; then 
-echo "The program only takes two arguments: the directory to check and the number of lines to print."
-exit 3
-fi
 
 DIR="."
 N_LINES=0
+FILE_NAMES=""
 
-for arg in $@; do
-check_args $arg
-done
-
-NUM_FILES=$(find $DIR -name "*.fasta" -or -name "*.fa" -type f 2>/dev/null| wc -l) 
-if [[ NUM_FILES == 0 ]]; then echo "I haven't found any fasta files in this directory."
-elif [[ NUM_FILES == 1 ]]; then echo "I have found 1 fasta file in this directory."
-else echo "I have found" $NUM_FILES "fasta files in this directory."
+# Here we check that the provided arguments are adequate:
+#   1. That there are no more than 2 arguments.
+#   2. That the arguments are a directory and/or a number. 
+if [[ $# -gt 2 ]]; then 
+    echo "The program only takes two arguments: the directory to check and the number of lines to print."
+    exit 1
+    else
+        for arg in $@; do
+            check_args $arg
+        done
 fi
+
+# Here we obtain the number of FASTA files in our directory. 
+NUM_FILES=$(find $DIR -name "*.fasta" -or -name "*.fa" -type f 2>/dev/null | wc -l) 
+if [[ NUM_FILES -eq 0 ]]; then 
+    echo "I haven't found any fasta files in this directory."
+    exit 0
+    elif [[ NUM_FILES -eq 1 ]]; 
+        then echo "I have found 1 fasta file in this directory."
+    else 
+        echo "I have found" $NUM_FILES "fasta files in this directory."
+fi
+
+# Here we obtain a list with the names of the readable FASTA files 
+for FILE in $(find $DIR -name "*.fasta" -or -name "*.fa" -type f 2>/dev/null); do 
+    if [[ -r $FILE ]]; then 
+        FILE_NAMES+="$FILE "
+    fi 
+done 
+
+# Here we get the unique IDs from our files 
+FASTA_ID=$(cat $FILE_NAMES | grep ">" | sort | uniq -c | wc -l)
+echo "There are a total of" $FASTA_ID "unique FASTA IDs"
+
+
